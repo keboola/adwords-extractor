d7202977d84b58b6283af2cd2ed6cd7fec9d44ee
Vladimír Kriška <vlado@keboola.com>
2017-01-02 15:34:31 +0100

    Remove custom docker-compose version and -e flag from docker login

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag keboola/adwords-extractor quay.io/keboola/adwords-extractor:$TRAVIS_TAG
 docker images
 docker push quay.io/keboola/adwords-extractor:$TRAVIS_TAG
