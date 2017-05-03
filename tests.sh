c7d9135ffd77f172861e232e11cf367b6389f05d
Jakub Matejka <jakub@keboola.com>
2017-05-03 22:18:50 +0200

    :bug: Fix docker setup

--- tests.sh
+++ tests.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 php --version \
-  && php composer.phar --version \
   && ./vendor/bin/phpcs --standard=psr2 -n --ignore=vendor . \
   && ./vendor/bin/phpstan analyse -l 5 src tests \
   && ./vendor/bin/phpunit -c ./phpunit.xml.dist
