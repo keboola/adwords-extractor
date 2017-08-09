e38d60e6ed8a121bf751912be5c0deda4a28ef8c
Jakub Matejka <jakub@keboola.com>
2017-08-09 10:22:32 +0200

    :tv: Remove deploy to Quay

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/adwords-extractor quay.io/keboola/adwords-extractor:$TRAVIS_TAG
-docker images
-docker push quay.io/keboola/adwords-extractor:$TRAVIS_TAG
-
 export APP_ID=keboola.ex-adwords-v201705
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola $APP_ID`
